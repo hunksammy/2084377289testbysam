110b911610ff88fb11eb9ee34f8955e194ba6690
hunksammy <77620363+hunksammy@users.noreply.github.com>
2021-03-02 17:21:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -57,6 +57,8 @@ var myApp = angular.module('myApp', ['ngRoute'])
   var contactList = [
   						
 						
+						
+						
 {id: 1, name: '	AKALTARA	', 				
 customer1: '	VINOD GENERAL (AKALTARA)	', amount1: ' TOYS -	2186	',  url1: '		',
 customer2: '	CHANDAN GENERAL (AKALTARA)	', amount2: '	5753	',  url2: '	/2084377289testbysam/AKALTARA/CHANDAN%20GENERAL%20(AKALTARA)%2029.htm	',
@@ -122,6 +124,62 @@ customer6: '	PUSTAK BHAWAN (BILASPUR)	', amount6: '	0	',  url6: '		',
 customer7: '	SEEMA SHRIVASTAV (BILASPUR)	', amount7: '	0	',  url7: '		',
 customer8: '	SONI GIFT (DEVIKHURDH) (BSP)	', amount8: '	810	',  url8: '		',
 						},
+{id: 1, name: '	BILHA	', 				
+customer1: '	PAYAL FANCY (BILHA)	', amount1: ' 	1059	',  url1: '	/2084377289testbysam/BILHA/PAYAL%20FANCY%20(BILHA)%2066.htm	',
+customer2: '	KISAN MEGA BIG BAZAR (BILHA)	', amount2: '	4470	',  url2: '	/2084377289testbysam/BILHA/KISAN%20MEGA%20BIG%20BAZAR%20(BILHA)%2031.htm	',
+customer3: '	RAJA DAILY NEEDS (BILHA)	', amount3: '	3529	',  url3: '	/2084377289testbysam/BILHA/RAJA%20DAILY%20NEEDS%20(BILHA).htm	',
+						},
+						
+{id: 1, name: '	BIRRA	', 				
+customer1: '	TOMESH (BIRRA)	', amount1: '	0	',  url1: '	/2084377289testbysam/BIRRA/TOMESH%20(BIRRA).htm	',
+						},
+{id: 1, name: '	BOADTARA	', 				
+customer1: '	PRIYAL GENERAL (BODTARA)	', amount1: ' 	29780	',  url1: '	https://drive.google.com/file/d/1hNGyEHqRAoTcF2xWYan4-9mEWs2R-STs/view?usp=sharing	',
+customer2: '	MONIKA FANCY STORE (BOADTARA)	', amount2: '	6649	',  url2: '		',
+customer3: '	PUNNET SAHU  (BOADTARA)	', amount3: '	6067	',  url3: '		',
+						},
+{id: 1, name: '	CHAKKARBHATA	', 				
+customer1: '	MAHAMAYA THRED HOUSE (CHBT)	', amount1: ' 	1586	',  url1: '	/2084377289testbysam/CHAKKARBHATA/MAHAMAYA%20THREAD%20(CHAKKARBHATA)%20103.htm	',
+customer2: '	MAHESH STD (CHBT)	', amount2: '	4856	',  url2: '	/2084377289testbysam/CHAKKARBHATA/MAHESH%20STD%20(CHAKKARBHATA)%20161.htm	',
+customer3: '	NEW JAI MATA DI  (CHBT)	', amount3: '	13727	',  url3: '	/2084377289testbysam/CHAKKARBHATA/NEW%20JAIMATA%20DI%20(CHAKKARBHATA)%2081.htm	',
+customer4: '	BABA GENERAL (CHBT)	', amount4: ' 	3	',  url4: '	/2084377289testbysam/CHAKKARBHATA/BABA%20GENERAL%20(CHAKKARBHATA)%20%20188.htm	',
+customer5: '	LAXMI KITABGHAR (CHBT)	', amount5: '	0	',  url5: '	/2084377289testbysam/CHAKKARBHATA/LAXMI%20KITAB%20GHAR%20(CHAKKARBHATA).htm	',
+customer6: '	SIDDARTH (CHBT)	', amount6: '	0	',  url6: '	/2084377289testbysam/CHAKKARBHATA/SIDDARTH%20(CHAKKARBHATA)%20139.htm	',
+						},
+{id: 1, name: '	CHAMPA	', 				
+customer1: '	AMIT SHRINGAR AND GIFT (CHAMPA)	', amount1: ' 	4	',  url1: '	<b><br>TOYS -	',
+customer2: '	SOHAM GIFT (CHAMPA)	', amount2: '	229	',  url2: '	<b><br>TOYS -	',
+customer3: '	HEMA SHRINGAR (CHAMPA)	', amount3: '	0	',  url3: '	<b><br>TOYS -	',
+customer4: '	RANI GENERAL (CHAMPA)	', amount4: ' 	0	',  url4: '	<b><br>TOYS -	',
+customer5: '	SHRINGAR SADAN (CHAMPA)	', amount5: '	20	',  url5: '	<b><br>TOYS -	',
+customer6: '	RADHA SWAMI (CHAMPA)	', amount6: '	0	',  url6: '	<b><br>TOYS -	',
+customer7: '	MOHSIN (CHAMPA)	', amount7: '	3061	',  url7: '	<b><br>TOYS -	',
+customer8: '	MAA BHAGWATI (CHAMPA)	', amount8: '	25157	',  url8: '	<b><br>TOYS -	',
+customer9: '	POOJA NOVELTY (CHAMPA)	', amount9: '	0	',  url9: '	<b><br>TOYS -	',
+customer10: '	KANHA SUPERBAZAR (CHAMPA)	', amount10: '	1	',  url10: '	<b><br>TOYS -	',
+						},
+{id: 1, name: '	CHURRI	', 				
+customer1: '	NEHA GENERAL (CHURI)	', amount1: ' 	3103	',  url1: '	/2084377289testbysam/CHURI/NEHA%20(CHURRI)%2071.htm	',
+customer2: '	MAA KAUSA GAI (CHURRI)	', amount2: '	0	',  url2: '	/2084377289testbysam/CHURI/MAA%20KAUSA%20GAI%20(CHURRI)%20292.htm	',
+						},
+						
+{id: 1, name: '	DABRA	', 				
+customer1: '	FASHION PARK (DABRA)	', amount1: ' 	9028	',  url1: '	<b><br>TOYS -	',
+						},
+{id: 1, name: '	DHARAMJAYGARH	', 				
+customer1: '	AKANKSHA  (DHARAMJAYGARH)	', amount1: ' 	0	',  url1: '	<b><br>TOYS -	',
+						},
+						
+{id: 1, name: '	DIPKA	', 				
+customer1: '	KOLKATA MAHASALE (DIPKA)	', amount1: ' 	34786	',  url1: '	/2084377289testbysam/DIPKA/KOLKATA%20MAHA%20SALE%20(DIPKA)%2048.htm	',
+customer2: '	NEW BOOK DEPO (DIPKA)	', amount2: '	2	',  url2: '	/2084377289testbysam/DIPKA/NEW%20BOOK%20DEPO.%20(DIPKA)%20110.htm	',
+customer3: '	GEETA BOOK DEPO (DIPKA)	', amount3: '	0	',  url3: '	/2084377289testbysam/DIPKA/GEETA%20BOOK%20DEPO.%20(DIPKA)%20266.htm	',
+customer4: '	SHANTI SUHAG (DIPKA)	', amount4: ' 	1371	',  url4: '	/2084377289testbysam/DIPKA/SHANTI%20SUHAG%20(DIPKA)%20129.htm	',
+customer5: '	KHUSHI BENGALS (DIPKA)	', amount5: '	-1	',  url5: '	/2084377289testbysam/DIPKA/KHUSHI%20BENGALS%20(DIPKA)%204.htm	',
+customer6: '	VASU COLLECTION  (DIPA)	', amount6: '	1597	',  url6: '	/2084377289testbysam/DIPKA/VASU%20COLLECTION.%20(DIPKA)%20149.htm	',
+						},
+
+
 
 
 
