e530cd4f3ff2d8e165eaae5258d84d26ed658fbf
hunksammy <77620363+hunksammy@users.noreply.github.com>
2021-03-25 14:12:57 +0530

    t

--- transport.js
+++ transport.js
@@ -4,13 +4,24 @@ app.controller("myCtrl", function($scope) {
     $scope.transportNumber1 = "9301489299";
 	$scope.transportName2 = "COOLIE BROTHERS";
     $scope.transportNumber2 = "97705030946";
+		$scope.transportName3 = "";
+    $scope.transportNumber3 = "";
+	$scope.transportName4 = "";
+    $scope.transportNumber4 = "";
 	
 	
-	$scope.area1 = "MASTURI";
+	$scope.area1 = "MASTURI" ; 
 	$scope.area2 = "JAIRAM NAGAR";
 	$scope.area3 = "MALHAR";
 	$scope.area4 = "PACHPEDI";
 	$scope.area5 = "MULMULA";
 	$scope.area6 = "NARIYARA";
+	$scope.area7 = "AKALTARA";
+	$scope.area8 = "NAILA JANJGIR";
+	$scope.area9 = "CHAMPA";
+	$scope.area10 = "SARGAON";
+	$scope.area11 = "BARADWAR";
+	$scope.area12 = "SAKTI";
+	$scope.area13= "MALKHARODA";
 	
 });
