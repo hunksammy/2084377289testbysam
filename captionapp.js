3f52c6bedb5a07b95e27589b33d29cf5ac41f356
hunksammy <77620363+hunksammy@users.noreply.github.com>
2021-05-07 15:59:53 +0530

    t

--- captionapp.js
+++ captionapp.js
@@ -5,12 +5,8 @@ const endpoints="https://raw.githubusercontent.com/hunksammy/2084377289testbysam
 fetch(endpoints)
  .then(blob => blob.json())
  .then(data => {
-<<<<<<< HEAD
  ul.innerHTML=data.map(place => `<li class="character">
                 <h2>${place.date}</div></h2><br> `).join("");
-=======
- ul.innerHTML=data.map(place => `<h1><div class="card">${place.date}</div></h1><br> `).join("");
->>>>>>> 6dcae78f90b8db044dc45e9759d625eca6df4b7a
 })
 
 
@@ -19,7 +15,6 @@ const charactersList = document.getElementById('charactersList');
 const searchBar = document.getElementById('searchBar');
 let hpCharacters = [];
 
-<<<<<<< HEAD
 searchBar.addEventListener('keyup', (e) => {
     const searchString = e.target.value.toLowerCase();
 
@@ -58,6 +53,3 @@ const displayCharacters = (characters) => {
 };
 
 loadCharacters();
-=======
-})
->>>>>>> 6dcae78f90b8db044dc45e9759d625eca6df4b7a
