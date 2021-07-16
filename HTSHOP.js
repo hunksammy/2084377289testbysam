35e8ec84d66e94265c7e222388ad9f0018adfba2
sameer motwani <77620363+hunksammy@users.noreply.github.com>
2021-07-17 00:08:53 +0530

    T

--- HTSHOP.js
+++ HTSHOP.js
@@ -14,6 +14,10 @@ $("#formLoc").submit(function(e){
   var x = {
     name:$('#nameLoc').val(),
     desc:$('#descLoc').val(),
+
+$("#formLoc").submit1(function(e){
+  e.preventDefault();
+  var x = {
     desc1:$('#nameLoc1').val(),
     name1:$('#descLoc1').val(),
 
