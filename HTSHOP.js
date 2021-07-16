e9a3eefce283702a9a2e5753339056d2f63c63c6
hunksammy <hindustantoysrs@gmail.com>
2021-07-16 19:49:52 +0530

    T

---
+++ HTSHOP.js
@@ -0,0 +1,19 @@
+var root = new Firebase('https://notes-12519-default-rtdb.firebaseio.com/');
+
+var locRef = root.child('location');
+locRef.on('child_added', function(snap){
+  var locVal = snap.val();
+  $('#locList').append(
+  	"<li>"+locVal.name+" "+locVal.desc+"</li>"
+  );
+});
+
+
+$("#formLoc").submit(function(e){
+  e.preventDefault();
+  var x = {
+    name:$('#nameLoc').val(),
+    desc:$('#descLoc').val()
+  }; 
+  locRef.push(x);
+});
