3363475f240d22e97df752090fc5547da957f1e7
hunksammy <hindustantoysrs@gmail.com>
2021-07-20 14:46:53 +0530

    T

--- HTSHOP.js
+++ HTSHOP.js
@@ -30,64 +30,34 @@ $("#formLocCUST").submit(function(e){
 
 
 
-
-
-
-
-
-
-$("#formLoc").submit(function(e){
-  e.preventDefault();
-  var x = {
-    desc1:$('#nameLoc').val(),
-    name1:$('#descLoc').val(),
-
-
+$("#formLoc1).submit(function(e){	
+  e.preventDefault();	
+  var x = {	
+    desc1:$('#nameLoc1').val(),	
+    name1:$('#descLoc1').val(),	
 	
-  }; 
-
-
-
-
-
-
-  locRef.push(x);
-});
-
-
-
-
-
-
-
-$("#formLoc1").submit(function(e){
-  e.preventDefault();
-  var x = {
-    desc1:$('#nameLoc1').val(),
-    name1:$('#descLoc1').val(),
-
-
 	
-  }; 
+	
+  }; 	
+	
+	
+	
+	
+	
+	
+  locRef.push(x);	
+});	
 
 
 
-
-
-
-  locRef.push(x);
-});
-
-
-
-$("#formLoc2").submit(function(e){
+$("#formLoc2).submit(function(e){
   e.preventDefault();
   var x = {
     desc2:$('#nameLoc2').val(),
     name2:$('#descLoc2').val(),
 
 
-	
+
   }; 
 
 
@@ -99,15 +69,14 @@ $("#formLoc2").submit(function(e){
 });
 
 
-
-$("#formLoc3").submit(function(e){
+$("#formLoc3).submit(function(e){
   e.preventDefault();
   var x = {
     desc3:$('#nameLoc3').val(),
     name3:$('#descLoc3').val(),
 
 
-	
+
   }; 
 
 
@@ -119,14 +88,14 @@ $("#formLoc3").submit(function(e){
 });
 
 
-$("#formLoc4").submit(function(e){
+$("#formLoc4).submit(function(e){
   e.preventDefault();
   var x = {
     desc4:$('#nameLoc4').val(),
     name4:$('#descLoc4').val(),
 
 
-	
+
   }; 
 
 
@@ -138,14 +107,15 @@ $("#formLoc4").submit(function(e){
 });
 
 
-$("#formLoc5").submit(function(e){
+
+$("#formLoc5).submit(function(e){
   e.preventDefault();
   var x = {
     desc5:$('#nameLoc5').val(),
     name5:$('#descLoc5').val(),
 
 
-	
+
   }; 
 
 
@@ -157,14 +127,14 @@ $("#formLoc5").submit(function(e){
 });
 
 
-$("#formLoc6").submit(function(e){
+$("#formLoc6).submit(function(e){
   e.preventDefault();
   var x = {
     desc6:$('#nameLoc6').val(),
     name6:$('#descLoc6').val(),
 
 
-	
+
   }; 
 
 
@@ -176,14 +146,14 @@ $("#formLoc6").submit(function(e){
 });
 
 
-$("#formLoc7").submit(function(e){
+$("#formLoc7).submit(function(e){
   e.preventDefault();
   var x = {
     desc7:$('#nameLoc7').val(),
     name7:$('#descLoc7').val(),
 
 
-	
+
   }; 
 
 
@@ -194,15 +164,14 @@ $("#formLoc7").submit(function(e){
   locRef.push(x);
 });
 
-
-$("#formLoc8").submit(function(e){
+$("#formLoc8).submit(function(e){
   e.preventDefault();
   var x = {
     desc8:$('#nameLoc8').val(),
     name8:$('#descLoc8').val(),
 
 
-	
+
   }; 
 
 
@@ -213,15 +182,14 @@ $("#formLoc8").submit(function(e){
   locRef.push(x);
 });
 
-
-$("#formLoc9").submit(function(e){
+$("#formLoc9).submit(function(e){
   e.preventDefault();
   var x = {
     desc9:$('#nameLoc9').val(),
     name9:$('#descLoc9').val(),
 
 
-	
+
   }; 
 
 
@@ -233,14 +201,14 @@ $("#formLoc9").submit(function(e){
 });
 
 
-$("#formLoc10").submit(function(e){
+$("#formLoc10).submit(function(e){
   e.preventDefault();
   var x = {
     desc10:$('#nameLoc10').val(),
     name10:$('#descLoc10').val(),
 
 
-	
+
   }; 
 
 
@@ -250,5 +218,196 @@ $("#formLoc10").submit(function(e){
 
   locRef.push(x);
 });
+
+
+$("#formLoc11).submit(function(e){
+  e.preventDefault();
+  var x = {
+    desc11:$('#nameLoc11').val(),
+    name11:$('#descLoc11').val(),
+
+
+
+  }; 
+
+
+
+
+
+
+  locRef.push(x);
+});
+
+
+$("#formLoc12).submit(function(e){
+  e.preventDefault();
+  var x = {
+    desc12:$('#nameLoc12').val(),
+    name12:$('#descLoc12').val(),
+
+
+
+  }; 
+
+
+
+
+
+
+  locRef.push(x);
+});
+
+
+$("#formLoc13).submit(function(e){
+  e.preventDefault();
+  var x = {
+    desc13:$('#nameLoc13').val(),
+    name13:$('#descLoc13').val(),
+
+
+
+  }; 
+
+
+
+
+
+
+  locRef.push(x);
+});
+
+
+$("#formLoc14).submit(function(e){
+  e.preventDefault();
+  var x = {
+    desc14:$('#nameLoc14').val(),
+    name14:$('#descLoc14').val(),
+
+
+
+  }; 
+
+
+
+
+
+
+  locRef.push(x);
+});
+
+
+$("#formLoc15).submit(function(e){
+  e.preventDefault();
+  var x = {
+    desc15:$('#nameLoc15').val(),
+    name15:$('#descLoc15').val(),
+
+
+
+  }; 
+
+
+
+
+
+
+  locRef.push(x);
+});
+
+
+$("#formLoc16).submit(function(e){
+  e.preventDefault();
+  var x = {
+    desc16:$('#nameLoc16').val(),
+    name16:$('#descLoc16').val(),
+
+
+
+  }; 
+
+
+
+
+
+
+  locRef.push(x);
+});
+
+
+$("#formLoc17).submit(function(e){
+  e.preventDefault();
+  var x = {
+    desc17:$('#nameLoc17').val(),
+    name17:$('#descLoc17').val(),
+
+
+
+  }; 
+
+
+
+
+
+
+  locRef.push(x);
+});
+
+
+$("#formLoc18).submit(function(e){
+  e.preventDefault();
+  var x = {
+    desc18:$('#nameLoc18').val(),
+    name18:$('#descLoc18').val(),
+
+
+
+  }; 
+
+
+
+
+
+
+  locRef.push(x);
+});
+
+$("#formLoc19).submit(function(e){
+  e.preventDefault();
+  var x = {
+    desc19:$('#nameLoc19').val(),
+    name19:$('#descLoc19').val(),
+
+
+
+  }; 
+
+
+
+
+
+
+  locRef.push(x);
+});
+
+
+$("#formLoc20).submit(function(e){
+  e.preventDefault();
+  var x = {
+    desc20:$('#nameLoc20').val(),
+    name20:$('#descLoc20').val(),
+
+
+
+  }; 
+
+
+
+
+
+
+  locRef.push(x);
+});
+
+
 
 
