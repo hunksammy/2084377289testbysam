5751f528321cbc03aab3f708f8fc62b972a75eae
hunksammy <hindustantoysrs@gmail.com>
2021-07-20 14:30:56 +0530

    T

--- HTSHOP.js
+++ HTSHOP.js
@@ -55,23 +55,11 @@ $("#formLoc").submit(function(e){
 });
 
 
-$("#formLoc1").submit(function(e){
-  e.preventDefault();
-  var x = {
-    desc1:$('#nameLoc1').val(),
-    name1:$('#descLoc1').val(),
 
-
-	
-  }; 
+$("#formLoc1").submit(function(e){  e.preventDefault();var x = { desc1:$('#nameLoc1).val(),name1:$('#descLoc1).val(),};locRef.push(x);});
 
 
 
-
-
-
-  locRef.push(x);
-});
 
 
 
