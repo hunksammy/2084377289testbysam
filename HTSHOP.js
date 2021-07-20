7ce1467143515b392f5402e0c37c1a0ee79d4ea7
hunksammy <hindustantoysrs@gmail.com>
2021-07-20 15:06:00 +0530

    T

--- HTSHOP.js
+++ HTSHOP.js
@@ -260,3 +260,191 @@ $("#formLoc10").submit(function(e){
 });
 
 
+$("#formLoc11").submit(function(e){
+  e.preventDefault();
+  var x = {
+    desc11:$('#nameLoc11').val(),
+    name11:$('#descLoc11').val(),
+
+
+
+  }; 
+
+
+
+
+
+
+  locRef.push(x);
+});
+
+
+$("#formLoc12").submit(function(e){
+  e.preventDefault();
+  var x = {
+    desc12:$('#nameLoc12').val(),
+    name12:$('#descLoc12').val(),
+
+
+
+  }; 
+
+
+
+
+
+
+  locRef.push(x);
+});
+
+
+$("#formLoc13").submit(function(e){
+  e.preventDefault();
+  var x = {
+    desc13:$('#nameLoc13').val(),
+    name13:$('#descLoc13').val(),
+
+
+
+  }; 
+
+
+
+
+
+
+  locRef.push(x);
+});
+
+
+$("#formLoc14").submit(function(e){
+  e.preventDefault();
+  var x = {
+    desc14:$('#nameLoc14').val(),
+    name14:$('#descLoc14').val(),
+
+
+
+  }; 
+
+
+
+
+
+
+  locRef.push(x);
+});
+
+
+$("#formLoc15").submit(function(e){
+  e.preventDefault();
+  var x = {
+    desc15:$('#nameLoc15').val(),
+    name15:$('#descLoc15').val(),
+
+
+
+  }; 
+
+
+
+
+
+
+  locRef.push(x);
+});
+
+
+$("#formLoc16").submit(function(e){
+  e.preventDefault();
+  var x = {
+    desc16:$('#nameLoc16').val(),
+    name16:$('#descLoc16').val(),
+
+
+
+  }; 
+
+
+
+
+
+
+  locRef.push(x);
+});
+
+$("#formLoc17").submit(function(e){
+  e.preventDefault();
+  var x = {
+    desc17:$('#nameLoc17').val(),
+    name17:$('#descLoc17').val(),
+
+
+
+  }; 
+
+
+
+
+
+
+  locRef.push(x);
+});
+
+
+$("#formLoc18").submit(function(e){
+  e.preventDefault();
+  var x = {
+    desc18:$('#nameLoc18').val(),
+    name18:$('#descLoc18').val(),
+
+
+
+  }; 
+
+
+
+
+
+
+  locRef.push(x);
+});
+
+
+$("#formLoc19").submit(function(e){
+  e.preventDefault();
+  var x = {
+    desc19:$('#nameLoc19').val(),
+    name19:$('#descLoc19').val(),
+
+
+
+  }; 
+
+
+
+
+
+
+  locRef.push(x);
+});
+
+
+$("#formLoc20").submit(function(e){
+  e.preventDefault();
+  var x = {
+    desc20:$('#nameLoc20').val(),
+    name20:$('#descLoc20').val(),
+
+
+
+  }; 
+
+
+
+
+
+
+  locRef.push(x);
+});
+
