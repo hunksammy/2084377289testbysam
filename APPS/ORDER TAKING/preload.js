0df4fe38edc6777406852bb8b538c6c993507081
hunksammy <hindustantoysrs@gmail.com>
2021-07-24 18:35:57 +0530

    T

---
+++ APPS/ORDER TAKING/preload.js
@@ -0,0 +1,34 @@
+$(document).ready(function(e){
+  
+  // if you changed the usual .preloader class in html, change here too
+  var preloader = $(".preloader");
+  
+  $(window).load(function() {
+    // do something when whole page is done loading
+    // remove the timeout when youre copying this in your website
+    
+    setTimeout(function () {
+      showPage();
+    },3000);
+  });
+  
+  function showPage() {
+    // hides the preloader with a nice transition (css opacity)
+    // see line 85 in css
+    preloader.fadeOut("slow");
+  }
+  
+  /* ################## */
+  /* ################## */
+  /* ################## */
+  /* NO IMPORTANT STUFF */
+  /* ################## */
+  /* ################## */
+  /* ################## */
+  
+  var again = $("h3");
+  
+  again.stop().click(function(e){
+    location.reload(true);
+  });
+});
