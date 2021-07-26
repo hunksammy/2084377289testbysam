9c0adeaae85522e04e79c8aab4f59db273ea0c65
sameer motwani <77620363+hunksammy@users.noreply.github.com>
2021-07-27 00:59:44 +0530

    T

--- APPS/ORDER TAKING/dist/VERTICAL.js
+++ APPS/ORDER TAKING/dist/VERTICAL.js
@@ -1,3 +1,25 @@
+import { Component } from '@angular/core';
+import { Router, Event, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
+
+@Component({
+  selector: 'my-app',
+  template: '<router-outlet></router-outlet>'
+})
+export class AppComponent  {
+  constructor(
+    router: Router
+  ) {
+    router.events
+          .pipe(filter((routerEvent: Event) => routerEvent instanceof NavigationEnd))
+          .subscribe(() => window.scrollTo(0, 0));
+  }
+}
+
+
+
+
+
 angular.module('toolDemo', ['ui.bootstrap'])
 .controller('demoCtrl', function($scope) {
     $scope.showStatus = true;
