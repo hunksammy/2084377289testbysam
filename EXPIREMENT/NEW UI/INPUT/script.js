403a767c69f583739a18b542916c27f5900ffb3b
hunksammy <hindustantoysrs@gmail.com>
2021-08-05 15:55:20 +0530

    t

--- EXPIREMENT/NEW UI/INPUT/script.js
+++ EXPIREMENT/NEW UI/INPUT/script.js
@@ -1,20 +1,39 @@
-(function() {
-  var app = angular.module("myApp", []);
+var root = new Firebase('https://notes-12519-default-rtdb.firebaseio.com/');
 
-  app.controller('SideBarController', function(){
-     this.tab = 1;
+var locRef = root.child('location');
+locRef.on('child_added', function(snap){
+  var locVal = snap.val();
+  $('#locList').append(
+  	"<li>"+locVal.name+" "+locVal.desc+"</li>"
+  );
+});
 
-     this.setTab = function(newValue){
-       console.log("tab changed: "+this.tab);
-       this.tab = newValue;
-     };
 
-     this.isSet = function(tabName){
-       console.log("tab is set to: "+this.tab);
-       return this.tab === tabName;
-     };
-   });
-})();
-/*Testing dynamic inserted navigation item*/
-$(".nav").append("<li ng-class=\"{active:tc.isSet(4)}\"><a href ng-click=\"tc.setTab(4)\">Item Four</a></li>")
-$(".tabs").append(" <div id=\"three\" ng-show=\"tc.isSet(4)\" class=\"tab\"><h1>Item Four</h1></div>")
+
+
+
+
+
+
+
+
+
+
+
+$("#formLoc11").submit(function(e){
+  e.preventDefault();
+  var x = {
+    desc11:$('#nameLoc11').val(),
+    name11:$('#descLoc11').val(),
+
+
+
+  }; 
+
+
+
+
+
+
+  locRef.push(x);
+});
