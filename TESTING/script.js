9e0a0203f33799fb2be044ad94573ccfd524c092
hunksammy <hindustantoysrs@gmail.com>
2021-10-28 17:37:25 +0530

    R

---
+++ TESTING/script.js
@@ -0,0 +1,27 @@
+var login = false;
+checkLogin();
+function checkLogin(){
+
+login?($('#login').hide(),$('#logout').show()):($('#login').show(),$('#logout').hide())
+}
+
+$("#form").submit(function(){
+    login = true; // you have to apply this  line in success
+    checkLogin(); // you have to apply this  line in success
+    $('#myModal').modal('hide'); // you have to apply this  line in success
+   var formdatatest= $("#form").serializeArray()
+  $.ajax({
+   type: "POST",
+   url: "/your/url",
+    data: formdatatest,
+   success: function(html){    
+
+
+   },
+   error:function()
+   {
+
+   }
+  });
+return false;
+});
